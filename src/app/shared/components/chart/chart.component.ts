ac07a2092ce53f152beee988884f689e14fb3bfb
TinoMaster <90280253+TinoMaster@users.noreply.github.com>
2024-11-12 12:58:01 +0100

    application finished

--- src/app/shared/components/chart/chart.component.ts
+++ src/app/shared/components/chart/chart.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ClientiDto } from 'src/app/core/models/dto/clienti-dto.model';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  @Input() clienti!: ClientiDto[];
+  maxFatturaValue: number = 10000; // Definir un valor máximo de referencia para los porcentajes
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // Calcula el porcentaje de total de facturación basado en un valor máximo de referencia
+  getFatturaPercentage(totaleFatture?: number): number {
+    return totaleFatture
+      ? Math.min((totaleFatture / this.maxFatturaValue) * 100, 100)
+      : 0;
+  }
 }
